e4bb53c77bc7a09c5fbe7473db175f2e389dd141
MacDuki <pcmyact@hotmail.com>
2026-09-02 17:30:57 -0300

    progres

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,9 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        <Disciplines />
         <TrainingExperience />
         <Benefits />
-        <Disciplines />
         <SchedulePricing />
         <FAQ />
         <Location />
